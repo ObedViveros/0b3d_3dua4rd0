97ebf6276821e1f97f34152871e10352de11ef54
OBED EDUARDO VIVEROS GARCIA <19308051220717@cbtis122.edu.mx>
2021-12-01 23:36:32 +0000

    Initial commit from PHPSandbox

---
+++ resources/views/valida.blade.php
@@ -0,0 +1,14 @@
+</br>
+</br>
+</br>
+</br>
+</br>
+
+<center><big><b><h1>Felicidades se a creado con exito su cuenta</h1></b></big></center>
+    <head>
+<style>
+body {
+      background-color:pink;       
+}
+</style>
+</head>
